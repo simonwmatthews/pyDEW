f4d142b3744fe7e1d2cfb51d2fcc530d907805b9
Simon Matthews <simonwmatthews@gmail.com>
2022-10-06 17:24:42 +0000

    Added the DEW c code

---
+++ pyQ3/dew2019_coderfiles/AU_p_dew2019_calc.h
@@ -0,0 +1,28 @@
+
+const char *AU_p_dew2019_identifier(void);
+const char *AU_p_dew2019_name(void);
+const char *AU_p_dew2019_formula(void);
+const double AU_p_dew2019_mw(void);
+const double *AU_p_dew2019_elements(void);
+
+double AU_p_dew2019_g(double T, double P);
+double AU_p_dew2019_dgdt(double T, double P);
+double AU_p_dew2019_dgdp(double T, double P);
+double AU_p_dew2019_d2gdt2(double T, double P);
+double AU_p_dew2019_d2gdtdp(double T, double P);
+double AU_p_dew2019_d2gdp2(double T, double P);
+double AU_p_dew2019_d3gdt3(double T, double P);
+double AU_p_dew2019_d3gdt2dp(double T, double P);
+double AU_p_dew2019_d3gdtdp2(double T, double P);
+double AU_p_dew2019_d3gdp3(double T, double P);
+
+double AU_p_dew2019_s(double T, double P);
+double AU_p_dew2019_v(double T, double P);
+double AU_p_dew2019_cv(double T, double P);
+double AU_p_dew2019_cp(double T, double P);
+double AU_p_dew2019_dcpdt(double T, double P);
+double AU_p_dew2019_alpha(double T, double P);
+double AU_p_dew2019_beta(double T, double P);
+double AU_p_dew2019_K(double T, double P);
+double AU_p_dew2019_Kp(double T, double P);
+
